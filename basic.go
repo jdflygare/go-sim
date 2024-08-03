cc7ed0cd54b3683e7a8b870b1e9a86d82aafe4b5
Josh <jdflygare@gmail.com>
2024-08-03 12:58:34 +0930

    all done but density changes and scattering CS

--- basic.go
+++ basic.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"math"
 	"math/rand"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -28,9 +31,9 @@ const (
 	electronCharge = 1.602e-19            // Coulombs
 	kCoulomb       = 8.9875517873681764e9 // Coulomb's constant in N·m²/C²
 	pi             = 3.141592653589793
-	kc             = 2.30709e-28 // ec^2/(4 pi e0) in Joule Meter
-	threshold      = 500 * eVtoJ // cutoff in J
-	mbtom2         = 1e-31       // convert millibarn to m^2
+	kc             = 2.30709e-28  // ec^2/(4 pi e0) in Joule Meter
+	threshold      = 1000 * eVtoJ // cutoff in J
+	mbtom2         = 1e-31        // convert millibarn to m^2
 )
 
 // Constants for the Bosch-Hale formulas
@@ -77,9 +80,9 @@ type Particle struct {
 func initializeMaterial() *Material {
 	return &Material{
 		atoms: []Atom{
-			{name: "deuterium", density: 5.67e28, Z: 1, A: 2, m: 2.014 * AMUtoKG},
+			{name: "deuterium", density: 0, Z: 1, A: 2, m: 2.014 * AMUtoKG},
 			{name: "titanium", density: 5.67e28, Z: 22, A: 48, m: 47.867 * AMUtoKG},
-			{name: "tritium", density: 0.0, Z: 1, A: 3, m: 3.016 * AMUtoKG},
+			{name: "tritium", density: 5.67e28, Z: 1, A: 3, m: 3.016 * AMUtoKG},
 			{name: "hydrogen", density: 0.0, Z: 1, A: 1, m: 1.008 * AMUtoKG},
 			{name: "helium3", density: 0.0, Z: 2, A: 3, m: 3.016 * AMUtoKG},
 		},
@@ -89,7 +92,7 @@ func initializeMaterial() *Material {
 func initializeParticles(n int) []*Particle {
 	particles := make([]*Particle, n)
 	for i := range particles {
-		particles[i] = &Particle{position: 0.0, energy: 10000.0 * eVtoJ, Z: 1, A: 2, m: DeuteriumMass, scatteringEvents: 0, fusionReaction: -1}
+		particles[i] = &Particle{position: 0.0, energy: 150000.0 * eVtoJ, Z: 1, A: 2, m: DeuteriumMass, scatteringEvents: 0, fusionReaction: -1}
 	}
 	return particles
 }
@@ -130,41 +133,39 @@ func computeLosses(particle *Particle, material *Material) float64 {
 	avgLoss := 0.0
 
 	for _, atom := range material.atoms {
-		deBethe := 0.0
+		//deBethe := 0.0
 		deLindhardt := 0.0
-		eIz := 0.0
+		//eIz := 0.0
 
 		// Compute Bethe energy losses
 		// ionization potential eIz computed in Joules
-		if atom.Z < 13 {
-			eIz = (12 + (7 / float64(atom.Z))) * float64(atom.Z) * eVtoJ
-		} else {
-			eIz = (9.76 + (58.5 * math.Pow(float64(atom.Z), -1.19))) * float64(atom.Z) * eVtoJ
-		}
+		//if atom.Z < 13 {
+		//	eIz = (12 + (7 / float64(atom.Z))) * float64(atom.Z) * eVtoJ
+		//} else {
+		//	eIz = (9.76 + (58.5 * math.Pow(float64(atom.Z), -1.19))) * float64(atom.Z) * eVtoJ
+		//}
 		// From wikipedia bethe bloch (relativistic)
 		//BetaSq := (math.Pow(particle.energy, 2) + 2*math.Pow(speedOfLight, 2)*particle.energy*particle.m) / math.Pow(particle.energy+math.Pow(speedOfLight, 2)*particle.m, 2)
 		//newBethe := -4 * pi / (electronMass * math.Pow(speedOfLight, 2)) * atom.density * float64(atom.Z) * math.Pow(float64(particle.Z), 2) / BetaSq * math.Pow(kc, 2) * (math.Log(2*electronMass*math.Pow(speedOfLight, 2)*BetaSq/(eIz*(1-BetaSq)) - BetaSq))
 
-		// Bethe bloch From Nastasi 5.38
-		logVal := math.Log(4 * particle.energy * electronMass / (particle.m * eIz))
-		deBethe = -2 * pi * float64(particle.Z*particle.Z) * float64(atom.Z) * atom.density * math.Pow(kc, 2) * (particle.m / electronMass) * logVal / particle.energy
+		// Bethe bloch From Nastasi 5.38.  is only positive for Deuterium incident on metals at more than ~200 keV (for titanium) and ~600 for Pd
+		// logVal := math.Log(4 * particle.energy * electronMass / (particle.m * eIz))
+		// deBethe = 2 * pi * float64(particle.Z*particle.Z) * float64(atom.Z) * atom.density * math.Pow(kc, 2) * (particle.m / electronMass) * logVal / particle.energy
 
 		// Compute Lindhardt energy losses (comes out in eV/cm, must be converted to J/m)
 		// k comes out dimensionless, but M1 must be in AMU and E1 in keV. Also, the density is converted to cm^3
 		Se := 3.83 * math.Pow(float64(particle.Z), 7.0/6.0) * float64(atom.Z) / math.Pow(math.Pow(float64(particle.Z), 2.0/3.0)+math.Pow(float64(atom.Z), 2.0/3.0), 1.5) * math.Sqrt(particle.energy*(JtoeV/1000)/(particle.m/AMUtoKG))
-		deLindhardt = Se * 1e-15 * atom.density / 1e6 * eVtoJ * 100 //1e-15 is from nastasi, 1e6 is density to cm^3, and eVtoJ*10 is to convert from eV/cm to J/m
+		deLindhardt = Se * 1e-15 * atom.density / 1e6 * eVtoJ * 100 //1e-15 is from nastasi, 1e6 is density to cm^3, and eVtoJ*100 is to convert from eV/cm to J/m
 
 		// combine the energy losses for a smooth transition low to high energy
-		var deComp float64
-		if deBethe > 0 {
-			deComp = deLindhardt * deBethe / (deLindhardt + deBethe)
-		} else {
-			deComp = deLindhardt
-		}
-		avgLoss += atom.density / totalDensity(material) * deComp
+		// var deComp float64
+		// if deBethe > 0 {
+		//	deComp = deLindhardt * deBethe / (deLindhardt + deBethe)
+		//} else {
+		//	deComp = deLindhardt
+		//}
+		avgLoss += atom.density / totalDensity(material) * deLindhardt
 	}
-
-	//fmt.Printf("ep: %.5e, avgLoss: %.5e\n", particle.energy, avgLoss)
 
 	return avgLoss
 }
@@ -210,39 +211,40 @@ func chooseReaction(particle *Particle, interactionAtom *Atom) int {
 	return 0
 }
 
-func fusionCrossSection(particle *Particle, interactionAtom *Atom, material *Material) float64 {
+func fusionCrossSection(particle *Particle, interactionAtom *Atom) float64 {
 	currentReaction := chooseReaction(particle, interactionAtom)
 
-	eKev := particle.energy * JtoeV / 1000
+	eCMKev := interactionAtom.m / (interactionAtom.m + particle.m) * particle.energy * JtoeV / 1000
 	dummy1 := 0.0
 	dummy2 := 1.0
 
 	// Calculate the astrophysical factor S
 	for i := 0; i < 4; i++ {
-		dummy1 += A[currentReaction][i] * math.Pow(eKev, float64(i))
-		dummy2 += B[currentReaction][i] * math.Pow(eKev, float64(i+1))
+		dummy1 += A[currentReaction][i] * math.Pow(eCMKev, float64(i))
+		dummy2 += B[currentReaction][i] * math.Pow(eCMKev, float64(i+1))
 	}
 	sFus := dummy1 / dummy2
 	particle.fusionReaction = currentReaction
 
 	//fmt.Printf("fusionCS: %.5e", sFus*1e3/math.Exp(BG[currentReaction]/math.Sqrt(eKev)))
 	// Calculate and return the fusion cross-section in millibarn
-	return sFus * 1e3 / math.Exp(BG[currentReaction]/math.Sqrt(eKev)) * mbtom2
+	return sFus * 1e3 / (eCMKev * math.Exp(BG[currentReaction]/math.Sqrt(eCMKev))) * mbtom2
 }
 
 func scatteringCrossSection(interactionAtom *Atom, energy float64, material *Material) float64 {
-	return 2.5e-26
+	return 2.5e-22
 }
 
-func meanFreePath(scatteringCS float64, fusionCS float64, particle *Particle, material *Material) float64 {
+func meanFreePath(scatteringCS float64, fusionCS float64, material *Material) float64 {
 	totalDensity := totalDensity(material)
 
 	return 1.0 / ((scatteringCS + fusionCS) * totalDensity)
 }
 
-func runSimulation(nParticles int, wg *sync.WaitGroup) {
+func runSimulation(nParticles int, wg *sync.WaitGroup) []*Particle {
 	defer wg.Done()
 
+	particleStack := make([]*Particle, nParticles)
 	material := initializeMaterial()
 	particles := initializeParticles(nParticles)
 	total_density := totalDensity(material)
@@ -263,8 +265,12 @@ func runSimulation(nParticles int, wg *sync.WaitGroup) {
 
 				// fusion needs updated material parameters
 				material.mutex.Lock()
-				fusionCS := fusionCrossSection(particle, &interactionAtom, material)
+				fusionCS := 0.0
+				if interactionAtom.Z < 3 {
+					fusionCS = fusionCrossSection(particle, &interactionAtom)
+				}
 
+				//fmt.Printf("energy: %f, fusion CS: %0.5e\n", particle.energy*JtoeV/1000, fusionCS/mbtom2)
 				// check if fusion or scattering occurs
 				if rand.Float64() < fusionCS/(fusionCS+scatteringCS) {
 					// fusion happened
@@ -284,38 +290,88 @@ func runSimulation(nParticles int, wg *sync.WaitGroup) {
 				} else {
 					// scattering happened
 					//fmt.Printf("scattering happened\n")
+					//reset fusionreaction to -1
+					particle.fusionReaction = -1
 					particle.scatteringEvents += 1
 				}
 				material.mutex.Unlock()
 
 				//fmt.Printf("scatCS %.5e, fusCS %.5e\n", scatteringCS, fusionCS)
 				// Compute new location and energy
-				meanPath := meanFreePath(scatteringCS, fusionCS, particle, material)
+				meanPath := meanFreePath(scatteringCS, fusionCS, material)
+				// compute the step length change
+				stepLength := -meanPath * math.Log(rand.Float64())
 				losses := computeLosses(particle, material)
-				particle.energy -= losses * 10e-9
-				particle.position += meanPath
+				particle.energy -= losses * stepLength
+				particle.position += stepLength
 
 				// Print the particle's state
-				//fmt.Printf("Goroutine %d, Particle energy: %f, position: %f, ses: %d\n", id, particle.energy, particle.position, particle.scattering_events)
+				//fmt.Printf("Goroutine %d, Particle energy(keV): %f, position(nm): %0.3e, losses(keV/nm): %f, steplength(nm): %f\n", id, particle.energy*JtoeV/1000, particle.position*1e9, losses*JtoeV/1000*1e-9, stepLength*1e9)
 			}
+
+			particleStack[id] = particle
 
 		}(particles[i], i)
 	}
 
 	particleWg.Wait()
 	fmt.Printf("fusion events %d\n", fusions)
+
+	return particleStack
 }
 
 func main() {
 	start := time.Now()
-	rand.Seed(time.Now().UnixNano())
+	//rand.Seed(time.Now().UnixNano())
 	var wg sync.WaitGroup
-	nParticles := 1_000_000
+	nParticles := 2_000_000
 
 	wg.Add(1)
-	go runSimulation(nParticles, &wg)
+	particleStack := runSimulation(nParticles, &wg) // Run the simulation and get the particle stack
 
 	wg.Wait()
 	duration := time.Since(start)
 	fmt.Printf("Total simulation time: %s\n", duration)
+
+	// Write the particle stack to a CSV file
+	filename := "particles.csv"
+	if err := writeParticlesToCSV(particleStack, filename); err != nil {
+		fmt.Printf("Error writing to CSV: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Particle data written to %s\n", filename)
+}
+
+func writeParticlesToCSV(particles []*Particle, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	header := []string{"Particle ID", "Energy (keV)", "Position (nm)", "Scattering Events", "Fusion Reactions"}
+	if err := writer.Write(header); err != nil {
+		return err
+	}
+
+	// Write particle data
+	for i, particle := range particles {
+		record := []string{
+			strconv.Itoa(i),
+			fmt.Sprintf("%0.2e", particle.energy*JtoeV/1000),
+			fmt.Sprintf("%0.2e", particle.position*1e9),
+			strconv.Itoa(particle.scatteringEvents),
+			strconv.Itoa(particle.fusionReaction),
+		}
+		if err := writer.Write(record); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
